9629d5dc745a39e0102979c0c3c9c96a4e3d0064
Farhan235-design <ffarhanaditya0@gmail.com>
2025-03-01 13:52:07 +0700

    praktikum2

---
+++ praktikum-2a/data-form-regis.php
@@ -0,0 +1,19 @@
+<?php
+$ar_prodi = [
+    'si' => 'Sistem Informasi',
+    'ti' => 'Teknik Informatika',
+    'bd' => 'Bisnis Digital',
+];
+
+$ar_skill = [
+    'html' => '10',
+    'css' => '10',
+    'javascript' => '20',
+    'RWD Bootstrap' => '20',
+    'php' => '30',
+    'phyton' => '30',
+    'java' => '50',
+];
+
+$ar_domisili = 
+["jakarta", "depok", "tangerang", "bekasi", "bogor", "lainnya"];
